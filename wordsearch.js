8af95a3b5285c953b74c83620a57410e6c882fa5
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-08-20 15:45:38 -0400

    Implement Word Search - Pair programming with @daraakitoye

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//Pair programming @akshathakulkarni and @daraakitoye
+
+const transpose = require('./transpose');
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return undefined;
     }
+    const transposed = transpose(letters);
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    const verticalJoin = transposed.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+
